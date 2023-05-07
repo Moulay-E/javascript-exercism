148486ace3326010afd62241080bcc99dbca99c8
Moulay-E <Moulay553E@gmail.com>
2023-05-07 14:39:11 +0200

    add a boucle for caps every first word

--- script.js
+++ script.js
@@ -1,23 +1,77 @@
 
 const exo1 = document.querySelector(".exo");
-let reverse = "hello world";
-let reverseFast = "that fast boy";
-function splits(word, fast){
-    //decoupe chaque elemetn un un index dans un tableau
-    let tab = word.split("");
-    console.log(tab);
-    //return element reversed from array in string
-    tab = tab.reverse();
-    console.log(tab + " ah");
-    // glue element (string) each other
-    tab = tab.join("");
-    console.log(tab + " ho");
-    //all in one
-    let theFast = fast.split("").reverse().join("");
+let words = "hello world of beautifull things";
+let theFast;
+//fonction qui decoupe retourne et assemble 
+function reversed(word){
+     theFast = word.split("").reverse().join("");
     console.log(theFast + 'hi');
-   return {tab: tab, theFast: theFast};
+   return theFast;
    
 
 };
-const result = splits(reverse, reverseFast);
-exo1.innerHTML= `reverse ${result.tab} ${result.theFast}`;
+const result = reversed(words);
+exo1.innerHTML= ` le mots non reversed : ${words} <br/> reverse:  ${theFast}`;
+
+const palindrome = document.createElement("p");
+palindrome.innerHTML = words == theFast ? 
+    "<br/>ce mots est un palindrome ": 
+    "ce mots n'est pas un palindrome";
+exo1.appendChild(palindrome);
+
+
+//----------exo de comparateur de longueur tableau--
+let longuestWordSplited;
+function longuestWord(word){
+   let splited = word.split(" ");
+    for (let i = 0; i < splited.length; i++){  
+        if(!longuestWordSplited || longuestWordSplited.split("").length < splited[i].split("").length){
+            longuestWordSplited = splited[i];
+        }
+    }
+    console.log(longuestWordSplited);
+   return longuestWordSplited;
+};
+
+//------autre method pour arriver a la meme chose : 
+// function longuestWord(word){
+//    let splited = word.split(" ");
+//    let arr = [];
+//     for (let i = 0; i < splited.length; i++){  
+//        arr.push(splited[i].length);
+//     }
+//     let resultat = Math.max(arr);
+//     console.log(resultat);
+//    return resultat;
+// };
+
+let resultLonguest = longuestWord(words);
+// console.log(words.split(" "));
+const longuestWordHtml = document.createElement("p");
+longuestWordHtml.innerHTML = `comparaison mots le plus long de ce texte:
+     ${words} <br/> et sont mots le plus long est : ${resultLonguest}`; 
+exo1.appendChild(longuestWordHtml);
+
+// --------- genere un nbr aléatoir -----------
+let random;
+function randomNumber(max){
+ return random = Math.round(Math.random() * max);
+}
+randomNumber(100);
+console.log(random);
+
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", function(){
+    btn.innerHTML = "Generer un nombre aléatoire: <br />"
+         + randomNumber(100);
+});
+
+// l'utilise pour selectionner dde maniere aleatoire un index tableau
+ const fruit =  ["bannae", "oranfe", "pomme"];
+function randomFruit(){
+    let fruitRandom = Math.round(Math.random() * fruit.length -1);
+    console.log(fruit[fruitRandom]);
+};
+randomFruit();
+
+
